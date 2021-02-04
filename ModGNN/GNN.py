54092cfe22f8f4dfc1be341114a43fd621c39f8d
Ryan Kortvelesy <rk627@cam.ac.uk>
2021-02-04 19:12:12 +0000

    refactoring

--- ModGNN/GNN.py
+++ ModGNN/GNN.py
@@ -14,22 +14,21 @@ class GNN(nn.Module):
 		self.network = list(map(lambda layer: GNNnode(**kwargs) if (layer is None) else layer, layers))
 		for layer in range(self.layers):
 			self.add_module("gnn_node_%d" % layer, self.network[layer])
-		self.forward_batch = self.forward
+
 
 	def forward(self, A, X):
-		# Reshape
-		A = A.to(device=device).double()
-		X = X.to(device=device).double()
-		batch, N, IN_DIM, K = X.shape; K -= 1
-		A = A.permute(0, 3, 1, 2) # A: batch x (K+1)+(nLayers-1) x N x N
-		X = X.permute(0, 3, 1, 2) # X: batch x (K+1)+(nLayers-1) x N x D
+		# A: batch x (K+1)+(nLayers-1) x N x N
+		# X: batch x (K+1)+(nLayers-1) x N x D
+		batch, N, Dobs, K = X.shape; K -= 1
 		# Multi Layer
 		layer_outputs = []
 		for layer in range(self.layers):
 			# Apply finput
 			layer_time = self.layers-1 - layer
-			Xc = self.network[layer].finput(X[:,layer_time:layer_time+self.K+1,:,:], *layer_outputs)
+			obs = X[:,layer_time:layer_time+self.K+1,:,:].view(batch * (K+1) * N, Dobs)
+			Xc = self.network[layer].finput(obs, *layer_outputs).view(batch, K+1, N, -1)
 			Ac = A[:,layer_time:layer_time+self.K,:,:]
+			Dinput = Xc.shape[-1]
 			# Calculate Y[k], the aggregated information from a k-hop neighbourhood
 			Y = {}
 			Y[1] = {-t: Xc[:,t,:,:] for t in range(1,self.K+1)}
@@ -39,12 +38,10 @@ class GNN(nn.Module):
 					yk = self.network[layer].fcom(Ac[:,k-1,:,:], Y[k-1][-t-1])
 					Y[k][-t] = yk.sum(dim=2)
 			# Calculate controller input Z
-			Dinput = Xc.shape[-1]
-			Z = torch.zeros((batch, self.K+1, N, N, Dinput), device=device).double()
-			Z[:,0,:,:,:] = torch.diag_embed(Xc[:,0,:,:].permute(0,2,1), dim1=1, dim2=2) # repeat local nonrel state info across dim2 (N)
+			Z = torch.zeros((batch, N, N, self.K+1, Dinput), device=Xc.device)
+			Z[:,:,:,0,:] = torch.diag_embed(Xc[:,0,:,:].permute(0,2,1), dim1=1, dim2=2)
 			for k in range(1,self.K+1):
-				Z[:,k,:,:,:] = self.network[layer].fcom(Ac[:,0,:,:], Y[k][-1])
-			self.last_Z = Z.permute(0,2,3,4,1)
+				Z[:,:,:,k,:] = self.network[layer].fcom(Ac[:,0,:,:], Y[k][-1])
 			# Apply the controller
 			output = self.network[layer].forward(Ac[:,0,:,:], Z)
 			layer_outputs.append(output)
--- ModGNN/GNNnode.py
+++ ModGNN/GNNnode.py
@@ -10,15 +10,16 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 
 class GNNnode(nn.Module):
 
-	def __init__(self, **kwargs):
+	def __init__(self, K=1, **kwargs):
 		super().__init__()
-		for fname, fgen in kwargs.items(): # example: GNN_Node(finput=finput_gen)
+		for fname, fgen in kwargs.items():
 			if fname in ['finput','fcom','fpre','fmid','ffinal']:
 				self.add_submodule(fname, fgen)
+		self.data = {k: [] for k in range(K+1)}
 
 	def finput(self, X, *layer_outputs):
-		# X: batch x K+1 x N x D_obs
-		# OUT: batch x K+1 x N x D_input
+		# X: (batch * (K+1) * N) x D_obs
+		# OUT: (batch * (K+1) * N) x D_input
 		return X
 	def fcom(self, A, X):
 		# A: batch x N x N
@@ -26,16 +27,16 @@ class GNNnode(nn.Module):
 		# OUT: batch x N x N x D_input
 		return (A[:,:,:,None] * X[:,None,:,:]) - (A[:,:,:,None] * X[:,:,None,:])
 	def fpre(self, X):
-		# X: batch x K+1 x N x N x D_input
-		# OUT: batch x K+1 x N x N x D_pre
+		# X: (batch * N * N) x K+1 x D_input
+		# OUT: (batch * N * N) x K+1 x D_pre
 		return X
 	def fmid(self, X):
-		# X: batch x K+1 x N x D_pre
-		# OUT: batch x K+1 x N x D_mid
+		# X: (batch * N) x K+1 x D_pre
+		# OUT: (batch * N) x K+1 x D_mid
 		return X
 	def ffinal(self, X):
-		# X: batch x N x D_mid
-		# OUT: batch x N x D_final
+		# X: (batch * N) x D_mid
+		# OUT: (batch x N) x D_final
 		return X
 
 	def add_submodule(self, fname, fgen):
@@ -61,20 +62,42 @@ class GNNnode(nn.Module):
 				self.add_module("%s_module_%d" % (name, i), layer)
 
 	def forward(self, A, X):
-		# A: batch x N x N
-		# X: batch x K+1 x N x N x D
-		batch, K, N, _, _ = X.shape; K -= 1
-		I = torch.eye(N, device=device).repeat(batch,1,1,1).double()
-		Ak = torch.cat([I, A.unsqueeze(1).expand(-1,K,-1,-1)], dim=1).unsqueeze(4) # Ak: batch x K+1 x N x N x 1
+		# A: batch x N x N (most recent adjacency matrix)
+		# X: batch x N x N x K+1 x D (last K+1 time steps of the joint state)
+		batch, N, _, K, _ = X.shape; K -= 1
+		I = torch.eye(N, device=device).repeat(batch,1,1,1)
+		Ak = torch.cat([I, A.unsqueeze(1).expand(-1,-1,-1,K)], dim=3).unsqueeze(4) # Ak: batch x N x N x K+1 x 1
 		# fpre
-		fpre_out = self.fpre(X) # fpre_out: batch x K+1 x N x N x D_pre
-		Z1 = (Ak * fpre_out).sum(dim=3) # Z1: batch x K+1 x N x Dpre
+		fpre_out = self.fpre(X.view(batch*N*N, K+1, -1)).view(batch, N, N, K+1, -1) # fpre_out: batch x N x N x K+1 x D_pre
+		Z1 = (Ak * fpre_out).sum(dim=3) # Z1: batch x N x K+1 x Dpre
 		# fmid
-		fmid_out = self.fmid(Z1) # fmid_out: batch x K+1 x N x D_mid
+		fmid_out = self.fmid(Z1.view(batch*N, K+1, -1)).view(batch, N, K+1, -1) # fmid_out: batch x N x K+1 x D_mid
 		Z2 = fmid_out.sum(dim=1) # Z2: batch x N x D_post
 		# ffinal
-		output = self.ffinal(Z2) # output: batch x N x D_final
+		output = self.ffinal(Z2.view(batch*N, -1)).view(batch, N, -1) # output: batch x N x D_final
 		return output
+
+
+	def input(self, x):
+		# x : batch x D or D
+		if len(x.shape) == 1:
+			x = x.unsqueeze(0)
+		self.data[0] = [self.finput(x)]
+
+
+	def receive(self, data):
+		pass
+
+
+	def send(self):
+		pass
+
+
+	def compute(self):
+		pass
+
+
+
 
 
 
